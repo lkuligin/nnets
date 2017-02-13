765314147cf5c6907b8165d10aa3e8879079ab86
Leonid Kugilin <lkuligin@yandex.ru>
2017-02-13 09:53:54 +0100

    udacity first week assignment

---
+++ udacityDL/NotMNIST/get_raw_data.py
@@ -0,0 +1,75 @@
+#!/Users/lkuligin/anaconda2/bin/python
+# coding: utf-8
+"""
+Data preparation
+"""
+
+import numpy as np
+import os
+import sys
+import tarfile
+from six.moves.urllib.request import urlretrieve
+
+URL = 'http://commondatastorage.googleapis.com/books1000/'
+LAST_PERCENT_REPORTED = None
+
+def download_progress_hook(count, blockSize, totalSize):
+  """A hook to report the progress of a download. This is mostly intended for users with
+  slow internet connections. Reports every 5% change in download progress.
+  """
+  global LAST_PERCENT_REPORTED
+  percent = int(count * blockSize * 100 / totalSize)
+
+  if LAST_PERCENT_REPORTED != percent:
+    if percent % 5 == 0:
+      sys.stdout.write("%s%%" % percent)
+      sys.stdout.flush()
+    else:
+      sys.stdout.write(".")
+      sys.stdout.flush()
+
+    last_percent_reported = percent
+
+def maybe_download(filename, expected_bytes, force=False):
+  """Download a file if not present, and make sure it's the right size."""
+  if force or not os.path.exists(filename):
+    print('Attempting to download:', filename) 
+    filename, _ = urlretrieve(URL + filename, filename, reporthook=download_progress_hook)
+    print('\nDownload Complete!')
+  statinfo = os.stat(filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', filename)
+  else:
+    raise Exception(
+      'Failed to verify ' + filename + '. Can you get to it with a browser?')
+  return filename
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
+
+if __name__ == "__main__":
+  train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+  test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+  num_classes = 10
+  np.random.seed(133)
+  train_folders = maybe_extract(train_filename)
+  test_folders = maybe_extract(test_filename)
